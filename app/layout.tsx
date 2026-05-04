a0134f75c654ff2534fa70b3b171f7f6457f2144
coderamrin <coderamrin@gmail.com>
2026-05-04 17:00:01 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Suspense fallback={null}>
           <FacebookPixel />
         </Suspense>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -10,27 +10,19 @@ export default function FacebookPixel() {
   useEffect(() => {
     if (!PIXEL_ID) return;
 
-    const init = () => {
-      if (typeof (window as any).fbq === 'function') return;
-      /* eslint-disable */
-      (function(f: any, b: any, e: any, v: any) {
-        if (f.fbq) return;
-        const n: any = f.fbq = function() { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments); };
-        if (!f._fbq) f._fbq = n;
-        n.push = n; n.loaded = true; n.version = '2.0'; n.queue = [];
-        const t = b.createElement(e); t.async = true; t.src = v;
-        const s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s);
-      })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-      /* eslint-enable */
-      (window as any).fbq('init', PIXEL_ID);
-      (window as any).fbq('track', 'PageView');
-    };
+    /* eslint-disable */
+    (function(f: any, b: any, e: any, v: any) {
+      if (f.fbq) return;
+      const n: any = f.fbq = function() { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments); };
+      if (!f._fbq) f._fbq = n;
+      n.push = n; n.loaded = true; n.version = '2.0'; n.queue = [];
+      const t = b.createElement(e); t.async = true; t.src = v;
+      const s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s);
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    /* eslint-enable */
 
-    const events = ['mousedown', 'touchstart', 'scroll'];
-    const handler = () => { init(); events.forEach(ev => window.removeEventListener(ev, handler)); };
-    const timer = setTimeout(init, 3000);
-    events.forEach(ev => window.addEventListener(ev, handler, { passive: true }));
-    return () => { clearTimeout(timer); events.forEach(ev => window.removeEventListener(ev, handler)); };
+    (window as any).fbq('init', PIXEL_ID);
+    (window as any).fbq('track', 'PageView');
   }, []);
 
   useEffect(() => {
